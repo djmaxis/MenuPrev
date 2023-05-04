3ad970b20e3f3b21ed168e81de678275828e085b
djmaxis <126213323+djmaxis@users.noreply.github.com>
2023-05-04 07:35:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,10 @@ $(document).ready(function () {
     $(".menu li a").hover(
         function () {
             var link = $(this).attr("href");
-            $.ajax({
-                url: link,
-                type: 'GET',
-                success: function (data) {
-                    $("#preview").html(data).show();
-                    $("#preview").css({
-                        top: $(this).offset().top + 30,
-                        left: $(this).offset().left + 10,
-                    });
-                },
-                error: function () {
-                    $("#preview").html('<p>Error al cargar la página</p>').show();
-                    $("#preview").css({
-                        top: $(this).offset().top + 30,
-                        left: $(this).offset().left + 10,
-                    });
-                }
+            $("#preview").load(link).show();
+            $("#preview").css({
+                top: $(this).offset().top + 30,
+                left: $(this).offset().left + 10,
             });
         },
         function () {
